4f1e68bfd32dd2c19515dfc1c9bc3ee5b354dd5f
Stainless-Nata <workingkeo@gmail.com>
2024-03-08 09:30:37 -0800

    remote footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           <ContentContainer>
             <Component {...pageProps} />
           </ContentContainer>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </ContextProvider>
     </>
